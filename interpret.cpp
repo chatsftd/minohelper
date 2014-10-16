491d2557c2f2f16dc04dbd96233f5679172ea6ab
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-10-16 18:57:48 +0900

    add interpret's parse

--- interpret.cpp
+++ interpret.cpp
@@ -1,13 +1,36 @@
 #include "interpret.h"
 #include <iostream>
+#include <sstream>
+#include <vector>
 using namespace std;
+
+void parse(vector<string>& vec,const string& a)
+{
+	stringstream input;
+	input.str(a);
+	string b; 
+	while(input >> b)
+	{
+		vec.push_back(b);
+	}
+}
 
 int interpret(const string& a)
 {
-	if(a == string("exit"))
+	typedef vector<string>::iterator VSI;
+	vector<string> vec;
+	
+	parse(vec,a);
+	
+	if(vec.empty()){return 0;}
+	
+	if(vec[0] == string("exit")){return -1;}
+	
+	int j=0;
+	for(VSI it = vec.begin(); it != vec.end(); ++it,++j)
 	{
-		return -1;
+		cout << "input #" << j << " is \"" << vec[j] << '"' << endl;
 	}
-	cout << "input is \"" << a << '"' << endl;
+	
 	return 0;
 }
