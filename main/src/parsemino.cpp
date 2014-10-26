75073e058e66ee679db6e34d68c959092fc7ee04
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-10-26 15:23:08 +0900

    add id to the input

--- main/src/parsemino.cpp
+++ main/src/parsemino.cpp
@@ -2,14 +2,32 @@
 #include <iostream>
 using namespace std;
 
+typedef int ID;
 data parse_mino(const vector<string>& vec)
 {
+	vector< vector< pair< ID, char> > > something;
+	ID id = 0;
 	for(size_t i = 0, n = vec.size(); i < n; i++)
 	{
+		vector< pair< ID, char> > tmp;
+		something.push_back(tmp);
 		for(size_t j = 0, m = vec[i].size(); j < m; j++)
 		{
-			cout << "(" << i+1 << ", " << j+1 << "): " << vec[i][j] << endl;
+			pair< ID,char> pic(id,vec[i][j]); 
+			id++;
+			something[i].push_back(pic);
 		}
 	}
+	
+	for(size_t i = 0, n = something.size(); i < n; i++)
+	{
+		for(size_t j = 0, m = something[i].size(); j < m; j++)
+		{
+			cout << "(" << i+1 << ", " << j+1 << "): " 
+				 << "(" << something[i][j].first << ",'" << something[i][j].second << "')" << endl;
+			
+		}
+	}
+	
 	return vec; // fixme
 }
