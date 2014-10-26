7bc8ac6a0780b570ff791b6139753cd8581ccaac
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-10-26 19:01:54 +0900

    actual union

--- main/src/parsemino.cpp
+++ main/src/parsemino.cpp
@@ -4,6 +4,7 @@ using namespace std;
 
 typedef int ID;
 
+
 static bool isSame(const vector< vector< pair<ID, char> > >& something, size_t i, size_t j, size_t i2, size_t j2)
 {
 	if(something.size() <= i  || something[i ].size() <= j ) return false;
@@ -18,6 +19,24 @@ static void print_inside(const vector< vector< pair<ID, char> > >& something, si
 {
 	cout << "(" << i+1 << ", " << j+1 << "): " 
 	 << "(" << something[i][j].first << ",'" << something[i][j].second << "')" << flush;
+}
+
+static ID root(const vector<ID>& union_find, ID id)
+{
+	ID parent_id;
+	while(true)
+	{
+		parent_id = union_find[id];
+		if(id == parent_id) return id;
+		id = parent_id;
+	}
+}
+
+static void unite(vector<ID>& union_find, const ID id1, const ID id2)
+{
+	ID root1 = root(union_find,id1);
+	ID root2 = root(union_find,id2);
+	union_find[root2] = root1;
 }
 
 data parse_mino(const vector<string>& vec)
@@ -45,13 +64,13 @@ data parse_mino(const vector<string>& vec)
 		union_find.push_back(i);
 	}
 	
-	for(size_t i = 0, n = something.size(); i < n; i++)
+	/*for(size_t i = 0, n = something.size(); i < n; i++)
 	{
 		for(size_t j = 0, m = something[i].size(); j < m; j++)
 		{
 			print_inside(something,i,j); cout << endl;
 		}
-	}
+	}*/
 	for(size_t i = 0, n = something.size(); i < n; i++)
 	{
 		for(size_t j = 0, m = something[i].size(); j < m; j++)
@@ -60,11 +79,13 @@ data parse_mino(const vector<string>& vec)
 			{
 				print_inside(something,i,j); cout << " and ";
 				print_inside(something,i,j-1); cout << " are the same." << endl;
+				unite(union_find,something[i][j].first,something[i][j-1].first);
 			}
 			if(isSame(something,i,j,i-1,j))
 			{
 				print_inside(something,i,j); cout << " and ";
 				print_inside(something,i-1,j); cout << " are the same." << endl;
+				unite(union_find,something[i][j].first,something[i-1][j].first);
 			}
 			
 			
