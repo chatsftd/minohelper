b2bd16264244976a9abd0386628f425959d2f4eb
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-10-29 17:09:24 +0900

    rename `something` to `atom_plane`

--- main/src/parsemino.cpp
+++ main/src/parsemino.cpp
@@ -5,32 +5,32 @@ using namespace std;
 typedef pair<ID, char> atom;
 
 
-static bool isSame(const vector< vector<atom> >& something, size_t i, size_t j, size_t i2, size_t j2)
+static bool isSame(const vector< vector<atom> >& atom_plane, size_t i, size_t j, size_t i2, size_t j2)
 {
-	if(something.size() <= i  || something[i ].size() <= j ) return false;
-	if(something.size() <= i2 || something[i2].size() <= j2) return false;
-	char a = something[i ][j ].second;
-	char b = something[i2][j2].second;
+	if(atom_plane.size() <= i  || atom_plane[i ].size() <= j ) return false;
+	if(atom_plane.size() <= i2 || atom_plane[i2].size() <= j2) return false;
+	char a = atom_plane[i ][j ].second;
+	char b = atom_plane[i2][j2].second;
 	if(a == ' ')return false;
 	return a == b;
 }
 
-static void print_inside(const vector< vector<atom> >& something, size_t i, size_t j)
+static void print_inside(const vector< vector<atom> >& atom_plane, size_t i, size_t j)
 {
-	cout << "(" << i+1 << ", " << j+1 << "): " << "(" << something[i][j].first << ",'" << something[i][j].second << "')" << flush;
+	cout << "(" << i+1 << ", " << j+1 << "): " << "(" << atom_plane[i][j].first << ",'" << atom_plane[i][j].second << "')" << flush;
 }
 
 data parse_mino(const vector<string>& vec)
 {
-	vector< vector<atom> > something;
+	vector< vector<atom> > atom_plane;
 	ID id = 0;
 	for(size_t i = 0, n = vec.size(); i < n; i++)
 	{
 		vector<atom> tmp;
-		something.push_back(tmp);
+		atom_plane.push_back(tmp);
 		for(size_t j = 0, m = vec[i].size(); j < m; j++)
 		{
-			something[i].push_back(atom(id,vec[i][j]));
+			atom_plane[i].push_back(atom(id,vec[i][j]));
 			id++;
 		}
 	}
@@ -45,25 +45,22 @@ data parse_mino(const vector<string>& vec)
 	
 	UnionFind union_find = vec2;
 	
-	for(size_t i = 0, n = something.size(); i < n; i++)
+	for(size_t i = 0, n = atom_plane.size(); i < n; i++)
 	{
-		for(size_t j = 0, m = something[i].size(); j < m; j++)
+		for(size_t j = 0, m = atom_plane[i].size(); j < m; j++)
 		{
-			if(isSame(something,i,j,i,j-1))
+			if(isSame(atom_plane,i,j,i,j-1))
 			{
-				print_inside(something,i,j); cout << " and ";
-				print_inside(something,i,j-1); cout << " are the same." << endl;
-				union_find.unite(something[i][j].first,something[i][j-1].first);
+				print_inside(atom_plane,i,j); cout << " and ";
+				print_inside(atom_plane,i,j-1); cout << " are the same." << endl;
+				union_find.unite(atom_plane[i][j].first,atom_plane[i][j-1].first);
 			}
-			if(isSame(something,i,j,i-1,j))
+			if(isSame(atom_plane,i,j,i-1,j))
 			{
-				print_inside(something,i,j); cout << " and ";
-				print_inside(something,i-1,j); cout << " are the same." << endl;
-				union_find.unite(something[i][j].first,something[i-1][j].first);
+				print_inside(atom_plane,i,j); cout << " and ";
+				print_inside(atom_plane,i-1,j); cout << " are the same." << endl;
+				union_find.unite(atom_plane[i][j].first,atom_plane[i-1][j].first);
 			}
-			
-			
-			
 		}
 	}
 	
