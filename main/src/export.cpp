2ab0ef0b8475025423142dbe900ea466d0509b32
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-11-12 19:25:52 +0900

    check if files are loaded

--- main/src/export.cpp
+++ main/src/export.cpp
@@ -5,12 +5,25 @@ using namespace std;
 status export_(state& st, const vector<string>& vec)
 {
 	cout << "command \"export\" was called." << endl;
+	size_t size = st.content.size();
+	
+	switch(size)
+	{
+		case 0:	
+			cout << "No files loaded." << endl << endl;	
+			return SOMETHING_WRONG;	
+			break; // consistency 
+			
+		default: 
+			break;
+	}
+
 	for(size_t j = 1; j < vec.size(); ++j)
 	{
 		cout << "argument #" << j << " is \"" << vec[j] << '"' << endl;
 	}
 	
-	cout << "Loaded files are:" << endl;
+	cout << (size == 1 ? "Loaded file is:" : "Loaded files are:") << endl;
 	for(map<string,data>::iterator it = st.content.begin(); it != st.content.end(); ++it)
 	{
 		cout << '\t' << it->first << endl;
