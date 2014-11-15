447ef894226593d00bc5c55b70c20115fe84a689
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-11-15 23:49:46 +0900

    separate file_select.cpp from export.cpp

--- main/src/export.cpp
+++ main/src/export.cpp
@@ -1,3 +1,4 @@
+#include "fileselect.h"
 #include "export.h"
 #include <iostream>
 using namespace std;
@@ -13,58 +14,7 @@ static status export2(const string& file, const vector<string>& vec)
 	return ALL_OK;
 }
 
-static status file_select(string& filename, const state& st)
-{
-	size_t size = st.content.size();
-	
-	switch(size)
-	{
-		case 0:	
-			cout << "No files loaded." << endl << endl;	
-			return SOMETHING_WRONG;	
-			break; // consistency 
-			
-		case 1:
-			filename = st.content.begin()->first;
-			return ALL_OK;
-			break; // consistency 
-		
-		default: 
-			break;
-	}
-	
-	cout << "Which file?" << endl;
-	typedef size_t Index;
-	
-	Index i = 1;
-	map<Index, string> map_;
-	for(map<string,data>::const_iterator it = st.content.begin(); it != st.content.end(); ++it)
-	{
-		cout << '\t' << i << ": \"" << it->first << '"' << endl;
-		map_[i] = it->first;
-		i++;
-	}
-	Index max_plus_1 = i;
-	
-	cout << ">>> " << flush;
-	
-	string str;
-	getline(cin, str);
-	
-	stringstream s2(str.c_str());
-	Index inp;
-	s2 >> inp;
-	if(inp == 0 || inp >= max_plus_1)
-	{
-		cout << "No such index." << endl;
-		return SOMETHING_WRONG;
-	}
-	else
-	{
-		filename = map_[inp];
-		return ALL_OK;
-	}
-}
+
 
 
 status export_(state& st, const vector<string>& vec)
