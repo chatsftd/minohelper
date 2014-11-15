c5e87642f8ad923bbbd55dc92bb0c3c99dadb2d5
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-11-16 07:09:04 +0900

    cleanup 12
    remove redundant #include
    change `inp == 0` to `inp < 1`

--- main/src/fileselect.h
+++ main/src/fileselect.h
@@ -2,7 +2,6 @@
 #define FILESELECT_H_9da9be7d61973eebc574e32b97db51cabedce429
 #include "header.h" 
 #include "state.h" 
-#include <vector>
 #include <string>
 
 status file_select(std::string& filename, const state& st);
