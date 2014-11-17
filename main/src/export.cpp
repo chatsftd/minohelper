2c5629132e0caf0d92f17ae19dc4c35bd56a5919
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-11-17 18:42:57 +0900

    pass `data` to `export2`

--- main/src/export.cpp
+++ main/src/export.cpp
@@ -3,12 +3,18 @@
 #include <iostream>
 using namespace std;
 
-static status export2(const string& file, const vector<string>& vec)
+static status export2(const string& file, const data& dat, const vector<string>& vec)
 {
 	cout << "Exporting \"" << file << "\"..." << endl;
 	for(size_t j = 1; j < vec.size(); ++j)
 	{
 		cout << "argument #" << j << " is \"" << vec[j] << '"' << endl;
+	}
+	
+	const vector<mino>& minos = dat.minos;
+	for(size_t i = 0; i < minos.size(); ++i)
+	{
+		cout << "mino #" << i+1 << ": " << minos[i] << endl;
 	}
 	
 	return ALL_OK;
@@ -23,7 +29,7 @@ status export_(state& st, const vector<string>& vec)
 	status s = file_select(filename, st);
 	if(s == ALL_OK)
 	{
-		return export2(filename,vec);
+		return export2(filename, st.content[filename], vec);
 	}
 	else
 	{
