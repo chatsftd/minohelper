24d1e27003ff8ba5a6dbd9c9766266c622e58f86
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-11-27 22:36:51 +0900

    separate maybe.h from header.h

--- main/src/import.h
+++ main/src/import.h
@@ -1,6 +1,7 @@
 #ifndef IMPORT_H_DEFINED_2014_10_12_ELEAFLRCWYMEMDNHQSDALBVAKKNMNM
 #define IMPORT_H_DEFINED_2014_10_12_ELEAFLRCWYMEMDNHQSDALBVAKKNMNM
 #include "header.h" 
+#include "maybe.h" 
 #include "state.h"
 #include <vector>
 #include <string>
---
+++ main/src/maybe.h
@@ -0,0 +1,35 @@
+#ifndef MAYBE_H_5d35ecd97713d038e9d8d4bff923d82939949a2c
+#define MAYBE_H_5d35ecd97713d038e9d8d4bff923d82939949a2c
+#include <cassert>
+template <class T>
+class Maybe
+{
+	bool isJust_;
+	T inside;
+	Maybe(bool ij, T t) : isJust_(ij), inside(t) {}
+	Maybe() : isJust_(false), inside() {}
+	
+public:
+	template <class T2> friend Maybe<T2> Nothing() { Maybe<T2> mt; return mt;}
+	template <class T2> friend Maybe<T2> Just(T2 t) { Maybe<T2> mt(true,t); return mt;}
+	bool isJust() const{ return this->isJust_; }
+	bool isNothing() const{ return !(this->isJust_); }
+	T unJust() const;
+};
+
+template <class T>
+T Maybe<T>::unJust() const
+{
+	if(this->isJust_) 
+	{
+		return this->inside; 
+	}
+	else
+	{
+		assert(0);
+		T t;
+		return t;
+	}
+}
+
+#endif/*MAYBE_H_5d35ecd97713d038e9d8d4bff923d82939949a2c*/
