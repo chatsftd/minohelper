03865a69402d63e485ea4b30c1d8dc4f49d18267
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-11-30 07:58:23 +0900

    separate import_file() from import_()

--- main/src/import.cpp
+++ main/src/import.cpp
@@ -5,39 +5,48 @@
 #include <sstream>
 using namespace std;
 
+static status import_file(state& st, const string& filename)
+{
+	cout << "Loading \"" << filename << "\" ..." << endl;
+	ifstream ifs(filename.c_str());
+	if(!ifs)
+	{
+		cerr << "file \"" << filename << "\" does not exist" << endl << endl;
+		return NO_FILE; // error
+	}
+	
+	vector<string> plane;
+	{
+		string s;
+		stringstream buffer;
+		buffer << ifs.rdbuf();
+		while(getline(buffer,s))
+		{
+			plane.push_back(s);
+		}
+	}
+	const Maybe<data>& mD = parse_mino(plane); 
+	
+	if(mD.isNothing())
+	{
+		return SOMETHING_WRONG;
+	}
+	st.content[filename] = mD.unJust(); 
+	cout << "OK, \"" << filename << "\" loaded." << endl << endl;
+	return ALL_OK;
+}
+
+
 status import_(state& st, const vector<string>& vec)
 {
 	status ret_val = ALL_OK;
 	for(size_t j = 1; j < vec.size(); ++j)
 	{
-		cout << "Loading \"" << vec[j] << "\" ..." << endl;
-		ifstream ifs(vec[j].c_str());
-		if(!ifs)
+		status s = import_file(st,vec[j]);
+		if(ret_val == ALL_OK)
 		{
-			cerr << "file \"" << vec[j] << "\" does not exist" << endl << endl;
-			ret_val = NO_FILE; // error
-			continue;
+			ret_val = s; // return the first error code
 		}
-		
-		vector<string> plane;
-		{
-			string s;
-			stringstream buffer;
-			buffer << ifs.rdbuf();
-			while(getline(buffer,s))
-			{
-				plane.push_back(s);
-			}
-		}
-		const Maybe<data>& mD = parse_mino(plane); 
-		
-		if(mD.isNothing())
-		{
-			ret_val = SOMETHING_WRONG;
-			continue;
-		}
-		st.content[vec[j]] = mD.unJust(); 
-		cout << "OK, \"" << vec[j] << "\" loaded." << endl << endl;
 	}
 	return ret_val;
 }
