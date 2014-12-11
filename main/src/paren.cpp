a5340d9549b3587a9587dbda319935df94235565
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-12-11 14:55:33 +0900

    ignore matched parentheses

--- main/src/paren.cpp
+++ main/src/paren.cpp
@@ -1,8 +1,18 @@
 #include "paren.h"
+#include "constructmino.h"
+#include <stack>
 using namespace std;
+
+enum Paren
+{
+	Parenthesis = ')', 
+	Bracket     = ']',
+	Brace       = '}'
+};
 
 status paren(state& st, vector<string>& plane)
 {
+	stack<Paren> paren_stack;
 	vector<string> plane2;
 	for(size_t i = 0, n = plane.size(); i < n; i++)
 	{
@@ -10,18 +20,26 @@ status paren(state& st, vector<string>& plane)
 		for(size_t j = 0, m = plane[i].size(); j < m; j++)
 		{
 			char c = plane[i][j];
-			plane2[i] += c;
+			bool ignore_me = false;
 			switch(c)
 			{
-				case '(': /*FALLTHROUGH*/
+				case '(': paren_stack.push(Parenthesis); break;
+				case '{': paren_stack.push(Brace      ); break;
+				case '[': paren_stack.push(Bracket    ); break;
+				
 				case ')': /*FALLTHROUGH*/
-				case '{': /*FALLTHROUGH*/
 				case '}': /*FALLTHROUGH*/
-				case '[': /*FALLTHROUGH*/
 				case ']':
-					cerr << "\"" << c << "\" is a reserved character and cannot be used to represent a mino." << endl << endl;
-					return SOMETHING_WRONG;
+					if(paren_stack.empty() || static_cast<char>(paren_stack.top()) != c)
+					{
+						cerr << "At " << point(i,j) << ": \"" << c << "\" is unmatched." << endl << endl;
+						return SOMETHING_WRONG;
+					}
+					paren_stack.pop(); 
+					ignore_me = true;
+					break;
 			}
+			plane2[i] += paren_stack.empty() && !ignore_me ? c : ' ';
 		}
 	}
 	plane = plane2;
