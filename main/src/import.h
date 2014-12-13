517b97c42361c77b720b595ed9ccd61a5634d808
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-12-13 15:12:57 +0900

    clean #include

--- main/src/import.h
+++ main/src/import.h
@@ -1,7 +1,6 @@
 #ifndef IMPORT_H_DEFINED_2014_10_12_ELEAFLRCWYMEMDNHQSDALBVAKKNMNM
 #define IMPORT_H_DEFINED_2014_10_12_ELEAFLRCWYMEMDNHQSDALBVAKKNMNM
 #include "status.h"
-#include "maybe.h"
 #include "state.h"
 #include <vector>
 #include <string>
