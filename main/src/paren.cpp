8b93c8bf08f129748f596a5549e8405ba453faae
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-12-14 19:31:28 +0900

    parse the meta info

--- main/src/paren.cpp
+++ main/src/paren.cpp
@@ -1,6 +1,9 @@
+#include "debug.h"
 #include "paren.h"
 #include "constructmino.h"
 #include <stack>
+#include <utility>
+#include <vector>
 using namespace std;
 
 enum Paren
@@ -10,7 +13,10 @@ enum Paren
 	Brace       = '}'
 };
 
-status paren(state& st, vector<string>& plane)
+typedef pair<Paren,string> meta;
+typedef vector<meta> SyntaxTree2; 
+
+static status paren2(SyntaxTree2& st, vector<string>& plane)
 {
 	stack<Paren> paren_stack;
 	vector<string> plane2;
@@ -23,9 +29,9 @@ status paren(state& st, vector<string>& plane)
 			bool ignore_me = false;
 			switch(c)
 			{
-				case '(': paren_stack.push(Parenthesis); break;
-				case '{': paren_stack.push(Brace      ); break;
-				case '[': paren_stack.push(Bracket    ); break;
+				case '(': if(paren_stack.empty()){ st.push_back(meta(Parenthesis,"")); } paren_stack.push(Parenthesis); break;
+				case '{': if(paren_stack.empty()){ st.push_back(meta(Brace      ,"")); } paren_stack.push(Brace      ); break;
+				case '[': if(paren_stack.empty()){ st.push_back(meta(Bracket    ,"")); } paren_stack.push(Bracket    ); break;
 				
 				case ')': /*FALLTHROUGH*/
 				case '}': /*FALLTHROUGH*/
@@ -39,9 +45,19 @@ status paren(state& st, vector<string>& plane)
 					ignore_me = true;
 					break;
 			}
+			
 			plane2[i] += paren_stack.empty() && !ignore_me ? c : ' ';
+			if(!paren_stack.empty()){ st[st.size()-1].second += c; }
 		}
 	}
 	plane = plane2;
 	return ALL_OK;
+}
+
+status paren(state& st, vector<string>& plane)
+{
+	SyntaxTree2 tree2;
+	status s = paren2(tree2,plane);
+	/* fixme: modify state using tree2*/
+	return s;
 }
