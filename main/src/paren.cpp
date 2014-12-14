971b79e992f97ca8fdb61075a199fcfece602487
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2014-12-14 19:39:39 +0900

    fix the parse of meta info
    output the meta info
    fix open parentheses being parsed twice

--- main/src/paren.cpp
+++ main/src/paren.cpp
@@ -13,6 +13,26 @@ enum Paren
 	Brace       = '}'
 };
 
+static char begin(Paren p)
+{
+	switch(p)
+	{
+		case Parenthesis: return '(';
+		case Bracket: return '[';
+		default: return '{';
+	}
+}
+
+static char end(Paren p)
+{
+	switch(p)
+	{
+		case Parenthesis: return ')';
+		case Bracket: return ']';
+		default: return '}';
+	}
+}
+
 typedef pair<Paren,string> meta;
 typedef vector<meta> SyntaxTree2; 
 
@@ -26,29 +46,31 @@ static status paren2(SyntaxTree2& st, vector<string>& plane)
 		for(size_t j = 0, m = plane[i].size(); j < m; j++)
 		{
 			char c = plane[i][j];
-			bool ignore_me = false;
+			bool ignore_close = false;
+			bool ignore_open  = false;
 			switch(c)
 			{
-				case '(': if(paren_stack.empty()){ st.push_back(meta(Parenthesis,"")); } paren_stack.push(Parenthesis); break;
-				case '{': if(paren_stack.empty()){ st.push_back(meta(Brace      ,"")); } paren_stack.push(Brace      ); break;
-				case '[': if(paren_stack.empty()){ st.push_back(meta(Bracket    ,"")); } paren_stack.push(Bracket    ); break;
+				case '(': if(paren_stack.empty()){ st.push_back(meta(Parenthesis,"")); } paren_stack.push(Parenthesis); ignore_open = true; break;
+				case '{': if(paren_stack.empty()){ st.push_back(meta(Brace      ,"")); } paren_stack.push(Brace      ); ignore_open = true; break;
+				case '[': if(paren_stack.empty()){ st.push_back(meta(Bracket    ,"")); } paren_stack.push(Bracket    ); ignore_open = true; break;
 				
 				case ')': /*FALLTHROUGH*/
 				case '}': /*FALLTHROUGH*/
 				case ']':
-					if(paren_stack.empty() || static_cast<char>(paren_stack.top()) != c)
+					if(paren_stack.empty() || end(paren_stack.top()) != c)
 					{
 						cerr << "At " << point(i,j) << ": \"" << c << "\" is unmatched." << endl << endl;
 						return SOMETHING_WRONG;
 					}
 					paren_stack.pop(); 
-					ignore_me = true;
+					ignore_close = true;
 					break;
 			}
 			
-			plane2[i] += paren_stack.empty() && !ignore_me ? c : ' ';
-			if(!paren_stack.empty()){ st[st.size()-1].second += c; }
+			if(!paren_stack.empty() && !ignore_open){ st[st.size()-1].second += c; }
+			plane2[i] += paren_stack.empty() && !ignore_close ? c : ' ';
 		}
+		if(!paren_stack.empty()){ st[st.size()-1].second += ' ';} // newline becomes space
 	}
 	plane = plane2;
 	return ALL_OK;
@@ -58,6 +80,11 @@ status paren(state& st, vector<string>& plane)
 {
 	SyntaxTree2 tree2;
 	status s = paren2(tree2,plane);
+	for(size_t i = 0, n = tree2.size(); i < n; i++)
+	{
+		cout << "meta #" << (i+1) << ": " << begin(tree2[i].first) << tree2[i].second << end(tree2[i].first) << endl;
+	}
+	
 	/* fixme: modify state using tree2*/
 	return s;
 }
