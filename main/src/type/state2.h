a74fbc447ab353c238d5e5041c34473fd3ffdfee
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2015-01-01 20:35:16 +0900

    parse color

--- main/src/type/state2.h
+++ main/src/type/state2.h
@@ -1,9 +1,25 @@
 #ifndef STATE2_H_DEFINED_1373E8A8C125AF49724367EE8ADFD116500B73AC
 #define STATE2_H_DEFINED_1373E8A8C125AF49724367EE8ADFD116500B73AC
-
-class state2
+#include <map>
+enum Color
 {
-	
+	RED,
+	ORANGE,
+	YELLOW,
+	GREEN,
+	LIGHTBLUE,
+	NAVY,
+	PURPLE,
+	BLACK,
+	WHITE,
+	HEAL
+};
+
+typedef std::map<char,Color> color_palette;
+
+struct state2
+{
+	color_palette palette;
 };
 
 #endif/*STATE2_H_DEFINED_1373E8A8C125AF49724367EE8ADFD116500B73AC*/
