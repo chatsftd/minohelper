cda46f0c3b8040d3908749dd30bef60ce012d536
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2015-01-21 05:40:12 +0900

    add `arguments2`, the classed `arguments`

--- main/src/type/args.cpp
+++ main/src/type/args.cpp
@@ -1,2 +1,22 @@
 #include "args.h"
+#include "../lib/debug.h"
 using namespace std;
+string& arguments2::operator [](size_t n)
+{
+#ifndef NDEBUG 
+	return this->inside.at(n);
+#else
+	return this->inside[n];
+#endif
+}
+
+string arguments2::operator [](size_t n) const
+{
+	string str;
+#ifndef NDEBUG 
+	str = this->inside.at(n);
+#else
+	str = this->inside[n];
+#endif
+	return str;
+}
--- main/src/type/args.h
+++ main/src/type/args.h
@@ -5,5 +5,18 @@
 
 typedef std::vector<std::string> arguments;
 
+class arguments2
+{
+	std::vector<std::string> inside;
+	
+public:
+	arguments2() : inside() {}
+	arguments2(arguments args) : inside(args) {}
+	void push_back(std::string s){ this->inside.push_back(s); }
+	bool empty() const{ return this->inside.empty(); }
+	size_t size() const{ return this->inside.size(); }
+	std::string& operator [](size_t n);
+	std::string operator [](size_t n) const;
+};
 
 #endif/*ARGS_H_INCLUDED_483B78994A97B2D564084F52B6E55D47721C1888*/
