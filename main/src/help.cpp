5e8db6682a50d2c27f8dd2113df7eaa3d079d822
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2015-02-05 22:15:30 +0900

    separate the help messages to another function

--- main/src/help.cpp
+++ main/src/help.cpp
@@ -16,6 +16,63 @@ static const char help[] = (
  "For more information, see 'help <command>'.\n"
  );
 
+static string each_help(const string& command)
+{ 
+	if(command == "import")
+	{
+		return( 
+			"usage: import [<files>]\n"
+			"  Import file(s) to minohelper\n"
+			"  If called with no arguments, it will ask for one.\n"
+			);
+	}
+	else if(command == "export")
+	{
+		return( 
+			"usage: export [<source>] [-o <destination>]\n"
+			"  Export file(s) from minohelper\n"
+			"  If called without destination, it will ask for one;\n"
+			"  if called without source, it will ask you to choose one.\n"
+			);
+	}
+	else if(command == "exit")
+	{
+		return( 
+			"usage: exit\n"
+			"  Exit minohelper\n"
+			);
+	}
+	else if(command == "status")
+	{
+		return( 
+			"usage: status\n"
+			"  Display minohelper's status\n"
+			"  Currently, this command displays the following things:\n"
+			"    loaded files\n"
+			"    error code of the last command\n"
+			);
+	}
+	else if(command == "help")
+	{
+		return( 
+			"usage: help [<command>]\n"
+			"  Display a command's detailed help\n"
+			"  If called with no arguments, it will display the list.\n"
+			);
+	}
+	else if(command == "config")
+	{
+		return( 
+			"usage: config --set <varname> <value>\n"
+			"       config --get <varname>\n"
+			"       config --list\n"
+			"  Set, get and list the config\n"
+			//"  If called with no arguments, it will display this help.\n"
+			);
+	}
+	else{ return "";}
+}  
+
 status help_(state& /*st**/, const arguments2& args)
 {
 	ret_data ret;
@@ -31,69 +88,15 @@ status help_(state& /*st**/, const arguments2& args)
 	}
 	vector<string> opt = opts[0];
 	string command = opt[1];
-	
-	if(command == "import")
-	{
-		cout << ( 
-			"usage: import [<files>]\n"
-			"  Import file(s) to minohelper\n"
-			"  If called with no arguments, it will ask for one.\n"
-			) << endl;
-		return ALL_OK;
-	}
-	else if(command == "export")
-	{
-		cout << ( 
-			"usage: export [<source>] [-o <destination>]\n"
-			"  Export file(s) from minohelper\n"
-			"  If called without destination, it will ask for one;\n"
-			"  if called without source, it will ask you to choose one.\n"
-			) << endl;
-		return ALL_OK;
-	}
-	else if(command == "exit")
-	{
-		cout << ( 
-			"usage: exit\n"
-			"  Exit minohelper\n"
-			) << endl;
-		return ALL_OK;
-	}
-	else if(command == "status")
-	{
-		cout << ( 
-			"usage: status\n"
-			"  Display minohelper's status\n"
-			"  Currently, this command displays the following things:\n"
-			"    loaded files\n"
-			"    error code of the last command\n"
-			) << endl;
-		return ALL_OK;
-	}
-	else if(command == "help")
-	{
-		cout << ( 
-			"usage: help [<command>]\n"
-			"  Display a command's detailed help\n"
-			"  If called with no arguments, it will display the list.\n"
-			) << endl;
-		return ALL_OK;
-	}
-	else if(command == "config")
-	{
-		cout << ( 
-			"usage: config --set <varname> <value>\n"
-			"       config --get <varname>\n"
-			"       config --list\n"
-			"  Set, get and list the config\n"
-			//"  If called with no arguments, it will display this help.\n"
-			) << endl;
-	}
-	else
+	string text = each_help(command);
+	if(text.empty())
 	{
 		cerr << "\"" << command << "\" is not a valid command." << endl; cout << endl;
 		return NO_SUCH_COMMAND;
 	}
-	
-	return ALL_OK;
+	else
+	{
+		cout << text << endl;
+		return ALL_OK;
+	}
 }
