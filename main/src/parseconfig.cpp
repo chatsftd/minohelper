867d4ded3c57c983185a3af43e8d007190d52114
CHAtsFtD <ex2fx1nx1btx2@hotmail.co.jp>
2015-04-06 23:15:45 +0900

    add function read_quoted_string() and quote()

--- main/src/parseconfig.cpp
+++ main/src/parseconfig.cpp
@@ -3,6 +3,27 @@
 #include <sstream>
 using namespace std;
 
+static istream& read_quoted_string(istream& is, string& res);
+static string quote(const string& str)
+{
+	string res = "\"";
+	for(size_t i = 0; i < str.size(); i++) {
+		switch(str[i]) {
+		case '\a': res += "\\a"; break;
+		case '\b': res += "\\b"; break;
+		case '\f': res += "\\f"; break;
+		case '\n': res += "\\n"; break;
+		case '\r': res += "\\r"; break;
+		case '\t': res += "\\t"; break;
+		case '\v': res += "\\v"; break;
+		case '\"': res += "\\\""; break;
+		case '\\': res += "\\\\"; break;
+		default: res.append(1,str[i]); break;
+		}
+	}
+	res += "\"";
+	return res;
+}
 
 ostream& operator<<(ostream& os, const config_value& val)
 {
@@ -62,6 +83,45 @@ istream& read_variable(istream& is, string& name)
 		} else { break; }
 	}
 	
+	return is;
+}
+
+static istream& read_quoted_string(istream& is, string& res)
+{
+	res = "";
+	is >> ws;
+	char q = is.peek();
+	if(q != '"' && q != '\'') { goto failure; }
+	is.get();
+	
+	while(!is.eof()) {
+		char c = is.get();
+		if(c == q) { return is; }
+		if(c != '\\') {
+			res.append(1,c);
+			continue;
+		}
+		
+		if(is.eof()) goto failure;
+		char c2 = is.get();
+		switch(c2) {
+		case 'a': res += "\a"; break;
+		case 'b': res += "\b"; break;
+		case 'f': res += "\f"; break;
+		case 'n': res += "\n"; break;
+		case 'r': res += "\r"; break;
+		case 't': res += "\t"; break;
+		case 'v': res += "\v"; break;
+		case '"': res += "\""; break;
+		case '\'': res += "'"; break;
+		case '\\': res += "\\"; break;
+		default: goto failure;
+		}
+	}
+	goto failure;
+	
+failure:
+	is.setstate(ios::failbit);
 	return is;
 }
 
